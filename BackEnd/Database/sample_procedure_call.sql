26de08ebd61ce97eac0d2d81767c18c1e4115d10
UmeshaJayakody <umesha.22@cse.mrt.ac.lk>
2024-10-30 14:27:16 +0530

    Update sample_procedure_call.sql to add new admins, supervisors, and employees

--- BackEnd/Database/sample_procedure_call.sql
+++ BackEnd/Database/sample_procedure_call.sql
@@ -1,9 +1,9 @@
--- add new employee
+-- add new admins
 call add_employee('7364528d-ce36-4d7f-a0aa-7cd084c077f3', 'Umesha','Jayakody', '1965-02-24', '949654341V', 'Male','Single', 0, '46,wakanda road,homagama.','0741234567','Umesha.jupiter@company.com','HR Manager','Full-time','HR', 'Headquarters',NULL,'Sahan','992912190V','46,wakanda road,homagama.','0712340123');
 call create_user_account('Umesha','Umesha','7364528d-ce36-4d7f-a0aa-7cd084c077f3','Admin');
 call add_employee('e10bfb2f-9cb9-4278-ac8c-952ee4428f02', 'Christopher','Ranasinghe', '1963-02-24', '929054321V', 'Male','Married', 2, '48, samagi road, Maharagema.','0751234567','Christopher.jupiter@company.com','HR assistent','Full-time','HR', 'Headquarters',NULL,'Shiranshi','992919190V','48, samagi road, Maharagema.','0710023452');
 call create_user_account('Christopher','Christopher','e10bfb2f-9cb9-4278-ac8c-952ee4428f02','Admin');
-
+-- add new supervisors
 call add_employee('7b163c45-ce7b-422f-93d9-d72abb4af5ae', 'Araliya','Perera', '1944-01-27', '989654321V', 'Male','Single', 0, '489, parakrame road, kottawa.','0751204567','Araliya.jupiter@company.com','Software enginner','Full-time','IT', 'Headquarters',NULL,'Kumara','923222222V','489, parakrame road, kottawa.','0718152643');
 call create_user_account('Araliya','Araliya','7b163c45-ce7b-422f-93d9-d72abb4af5ae','Supervisor');
 call add_employee('8c163c45-ce7b-422f-93d9-d72abb4af5ae', 'Nimal','Fernando', '1965-02-28', '989654322V', 'Male','Single', 0, '490, parakrame road, kottawa.','0751204568','Nimal.jupiter@company.com','Software enginner','Full-time','IT', 'Headquarters',NULL,'Kumara','923222223V','490, parakrame road, kottawa.','0718152644');
@@ -12,7 +12,7 @@ call add_employee('9d163c45-ce7b-422f-93d9-d72abb4af5ae', 'Sunil','Perera', '196
 call create_user_account('Sunili','Sunili','9d163c45-ce7b-422f-93d9-d72abb4af5ae','Supervisor');
 call add_employee('ad163c45-ce7b-422f-93d9-d72ab74af5ae', 'Kamal','Silva', '1967-04-30', '989654324V', 'Male','Single', 0, '492, parakrame road, kottawa.','0751204570','Kamal.jupiter@company.com','QA Engineer','Full-time','Main', 'Headquarters',NULL,'Kumara','923222225V','492, parakrame road, kottawa.','0718152646');
 call create_user_account('Kamali','Kamali','ad163c45-ce7b-422f-93d9-d72ab74af5ae','Supervisor');
-
+-- add new employees
 call add_employee('0ba6900a-c6bd-40ad-bd40-d335fbd42f8b', 'Nayana','Jayarathna', '1968-01-24', '989653329V', 'Female','Single', 0, '400,piliyandala road,pannipitiya.','0751288490','Aayana.jupiter@company.com','Worker','Full-time','IT', 'Headquarters',NULL,'Vibawa','922280690V','400,piliyandala road,pannipitiya.','0711214680');
 call create_user_account('Nayana','Nayana','0ba6900a-c6bd-40ad-bd40-d335fbd42f8b','Worker');
 call add_employee('21b63c70-5c83-433b-9ab8-de8899d18854', 'Upuli','Jayasinghe', '1960-01-24', '969653320V', 'Female','Married', 6, '91,Athurugiriya road,Malabe.','0752843628','Upuli.jupiter@company.com','Cleaner','Part-time','IT', 'Headquarters',NULL,'Namal','912340690V','91,Athurugiriya road,Malabe.','0711234580');
